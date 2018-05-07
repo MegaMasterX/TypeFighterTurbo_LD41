855e3a1b044a12c5dc84312bc805964ff7481985
MegaMasterX <megamasterx@outlook.com>
2018-05-07 11:47:06 -0400

    Add project files.

---
+++ Assets/Scripts/Game Cores/SoundEmitter.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class SoundEmitter : MonoBehaviour {
+
+	[SerializeField]
+	AudioClip KeyPress1;
+	[SerializeField]
+	AudioClip KeyPress2;
+	[SerializeField]
+	AudioClip KeyPress3;
+	[SerializeField]
+	AudioClip KeyPress4;
+	[SerializeField]
+	AudioClip KeyPress5;
+	[SerializeField]
+	AudioClip KeyPress6;
+	[SerializeField]
+	AudioClip KeyPress7;
+	[SerializeField]
+	AudioClip KeyPress8;
+	[SerializeField]
+	AudioClip KeyPress9;
+	[SerializeField]
+	AudioClip KeyPress10;
+	[SerializeField]
+	AudioClip KeyPress11;
+	[SerializeField]
+	AudioClip KeyPress12;
+	[SerializeField]
+	AudioClip KeyPress13;
+
+
+	void RandomizeSFX()
+	{
+		//Returns a randomized SFX in slot 1-13.
+		GameObject target;
+		if (SceneManager.GetActiveScene().name == "WarningScreen")
+		{
+			target = GameObject.Find("WarningLabel");
+		}else
+			target = GameObject.Find("SystemContainer");
+		
+		AudioSource emitter = target.GetComponent<AudioSource>();
+		int IndexReturned = (int)UnityEngine.Random.Range(1.0F, 13.0F);
+		switch (IndexReturned)
+		{
+			case 1:
+				emitter.clip = KeyPress1;
+				emitter.Play();
+				break;
+			case 2:
+				emitter.clip = KeyPress2;
+				emitter.Play();
+				break;
+			case 3:
+				emitter.clip = KeyPress3;
+				emitter.Play();
+				break;
+			case 4:
+				emitter.clip = KeyPress4;
+				emitter.Play();
+				break;
+			case 5:
+				emitter.clip = KeyPress5;
+				emitter.Play();
+				break;
+			case 6: 
+				emitter.clip = KeyPress6;
+				emitter.Play();
+				break;
+			case 7:
+				emitter.clip = KeyPress7;
+				emitter.Play();
+				break;
+			case 8:
+				emitter.clip = KeyPress8;
+				emitter.Play();
+				break;
+			case 9:
+				emitter.clip = KeyPress9;
+				emitter.Play();
+				break;
+			case 10:
+				emitter.clip = KeyPress10;
+				emitter.Play();
+				break;
+			case 11:
+				emitter.clip = KeyPress11;
+				emitter.Play();
+				break;
+			case 12:
+				emitter.clip = KeyPress12;
+				emitter.Play();
+				break;
+			case 13:
+				emitter.clip = KeyPress13;
+				emitter.Play();
+				break;
+			default:
+				break;
+		}
+		
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Text Combat/PlayerInput.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class PlayerInput : MonoBehaviour {
+
+	//The colors for text are supported in the following style:   <color=#hexcode>t</color> - This will need to be inserted before the typed character and </color> after to show richly. 
+	public string NextCharacter;
+	public string InputAlreadyEntered;
+
+	bool IsAcceptingInput = false; //Can the player type?
+
+	GameObject SystemContainer;
+	GameObject WordBox;
+	void Start()
+	{
+		SystemContainer = GameObject.Find("SystemContainer"); //Load the SystemContainer, you'll need it later.
+		WordBox = GameObject.Find("txtWordBox"); //Load the WordBox for updating the visuals if a press matches what its supposed to be or not.
+		
+	}
+
+
+	public void BeginGame()
+	{//This Method is going to be in all the appropriate systems that are working in tandem that denotes that the word list has been loaded in memory and the player can begin typing.
+		Debug.Log("Player Input is now being listened to.");
+		
+		IsAcceptingInput = true;
+	}
+
+	public void RoundFinished()
+	{
+		IsAcceptingInput = false;
+	}
+
+	void Update () {
+	//Handle input
+		if (IsAcceptingInput == true)
+		{
+			if (Input.GetKey(KeyCode.Return) == false && Input.GetKey(KeyCode.Backspace) == false)
+			{
+				if (Input.inputString != "")
+				{
+					GameObject.Find("txtWordBox").GetComponent<WordBox>().SendMessage("CheckLetter",Input.inputString);
+					GameObject.Find("playerInput").GetComponent<Text>().text += Input.inputString;
+					GameObject.Find("SystemContainer").GetComponent<PerSecondManager>().SendMessage("IncrementCharacters");
+				}
+				if (Input.inputString == " ")
+				{//The player reset the box.
+
+					//Reset the input box.
+					GameObject.Find("playerInput").GetComponent<Text>().text = "";
+					
+				}
+				
+			}
+				
+				
+			}
+				
+			
+
+	}
+	void LateUpdate()
+	{//Maintain the currently shown input.
+
+	}
+
+	void WordCompleted()
+	{
+
+	}
+}
